fcb03658f43226302b18eb61f9f497187ed0795d
Дмитрий Брилько <DimaBrilko@gmail.com>
2024-03-24 12:57:35 +0300

    Сделал сохранение

---
+++ PyTFEdu/CompressedPhoto.py
@@ -0,0 +1,10 @@
+class CompressedPhoto():
+    def __init__(self, compressedTiles, weights, shape, pixelsPerTile):
+        self.compressedTiles = compressedTiles
+        self.weights = weights
+        self.shape = shape
+        self.pixelsPerTile = pixelsPerTile
+
+
+
+
--- PyTFEdu/PyTFEdu.py
+++ PyTFEdu/PyTFEdu.py
@@ -2,6 +2,10 @@ from ConfigurationParameters.Pathes import path as p
 from ImageIO import imageIO as iIO
 from NpImageConverter import npImageConverter as ic
 from CompressionModel import compressionModel as model
+import pickle
+import numpy as np
+from CompressedPhoto import CompressedPhoto
+from ConfigurationParameters.Configuration import configuration as c
 
 npImage = iIO.readImage(p.source)
 
@@ -13,9 +17,22 @@ model.compile(chanels)
 
 model.fit(data)
 
-q = model.compressionModel.predict(data)
-w = model.restoreModel.predict(q)
-recovered = ic.recoverNpImage(w, shape)
+compressed = model.compressionModel.predict(data)
+
+compressed = np.array(compressed, dtype='float16')
+
+restoreWeights = model.restoreModel.weights
+
+compressedPhoto = CompressedPhoto(compressed, restoreWeights, shape, c.pixelsPerTile)
+
+with open('Encoded/there', 'wb') as f:
+    pickle.dump(compressedPhoto, f)
+    
+with open('Encoded/there', 'rb') as f:
+    e = pickle.load(f)
+
+restored = model.restoreModel.predict(compressed)
+recovered = ic.recoverNpImage(restored, shape)
 
 iIO.showImage(recovered)
 
